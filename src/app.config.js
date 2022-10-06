4f0448f5fdc57f8910c54b0195b62e70b509c62e
thgal <thgal@deloitte.com>
2022-10-06 16:53:38 +0000

    some message

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Test User6 change';
+    team = 'Test User6 change2';
     version = '1.0.1';
 }
 module.exports = AppConfig;
